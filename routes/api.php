919af95a01b6112998b49a9ac02813e99dcef0ba
DESKTOP-TOJLL8R\10Administrator <Josip.Maric@tri-m.hr>
2022-12-08 16:24:27 +0100

    test

--- routes/api.php
+++ routes/api.php
@@ -51,12 +51,13 @@ Route::middleware('auth:api')->post('search', [SearchController::class, 'index']
 Route::post('/testing', function (Request $request) {
     $tempRequest = $request->all();
     $temp = json_decode($request->get('objectJSON'));
-    /*if(key_exists('master', $temp)){
-        if($temp['master'] == 144){
+    if(key_exists('master', $temp)){
+        /*if($temp['master'] == 144){
             $temp['master'] = 128;
             $tempRequest['objectJSON'] = json_encode($temp);
-        }
-    }*/
+        }*/
+        $i = 0;
+    }
     Http::post('https://eoq5gro7kjr6leg.m.pipedream.net', [$tempRequest]);
     return 'yes';
 });
