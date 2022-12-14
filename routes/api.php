274eeed64247602d50b60389edef5d071a26b223
DESKTOP-TOJLL8R\10Administrator <Josip.Maric@tri-m.hr>
2022-12-14 11:54:29 +0100

    test

--- routes/api.php
+++ routes/api.php
@@ -80,7 +80,7 @@ Route::post('/testing', function (Request $request) {
             $tempRequest['objectJSON'] = json_encode($temp);
         }else if(!($temp->master & (1 << 2)) && !($temp->master & (1 << 1)) && $name == "tri-m-59fcc45e"){
             $temp->master = modifyBit($temp->master, 3,0);
-            $tempRequest['objectJSON'] = json_encode($temp);
+            $tempRequest['objectJSON'] = json_encode($temp);//test
         }
     }
     Http::post('https://campsabout.com/api/helium', $tempRequest);
